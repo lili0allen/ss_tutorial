b26c9928eafe33a17874ea64c415258200900e19
lili0allen <lili0allen@gmail.com>
2016-08-03 23:45:32 +1000

    move db connection to env

--- deploy.sh
+++ deploy.sh
@@ -22,7 +22,7 @@ else
 		#if [ "$f" != ".travis.yml" ] && [ "$f" != "deploy.sh" ] && [ "$f" != "test.js" ] && [ "$f" != "package.json" ]
 		#then
 	 		echo "Uploading $f"
-	 		curl --ftp-create-dirs -T $f -u $FTP_USER:$FTP_PASS ftp://easymode.com.au/$f
+	 		#curl --ftp-create-dirs -T $f -u $FTP_USER:$FTP_PASS ftp://easymode.com.au/$f
 		#fi
 	done
 fi
