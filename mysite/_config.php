2d3afd9210ce176ae12fb69e1755c3ac8e871a09
lili0allen <lili0allen@gmail.com>
2016-08-30 11:54:17 +1000

    add db config

--- mysite/_config.php
+++ mysite/_config.php
@@ -3,6 +3,16 @@
 global $project;
 $project = 'mysite';
 
+global $databaseConfig;
+$databaseConfig = array(
+    "type" => 'MySQLDatabase',
+    "server" => '127.0.0.1',
+    "username" => 'root',
+    "password" => 'password',
+    "database" => 'handy_service',
+    "path" => '',
+);
+
 // Set the site locale
 i18n::set_locale('en_US');
 //Security::SetDefaultAdmin('admin', 'admin');
