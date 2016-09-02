464efb4395a98bee7e8231e1d33f1ad6e5855893
lili0allen <lili0allen@gmail.com>
2016-09-02 16:21:05 +1000

    remove scripts

--- deploy.sh
+++ deploy.sh
@@ -19,10 +19,10 @@ else
     for f in $filesChanged
 	do
 		#do not upload these files that aren't necessary to the site
-		#if [ "$f" != ".travis.yml" ] && [ "$f" != "deploy.sh" ] && [ "$f" != "test.js" ] && [ "$f" != "package.json" ]
-		#then
+		if [ "$f" != ".travis.yml" ] && [ "$f" != "deploy.sh" ] && [ "$f" != "test.js" ] && [ "$f" != "package.json" ]
+		then
 	 		echo "Uploading $f"
-	 		#curl --ftp-create-dirs -T $f -u $FTP_USER:$FTP_PASS ftp://easymode.com.au/$f
-		#fi
+	 		curl --ftp-create-dirs -T $f -u $FTP_USER:$FTP_PASS ftp://easymode.com.au/$f
+		fi
 	done
 fi
